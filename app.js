e326440e7984b85ead9bbe8914fd7de8423d4865
root <root@LAPTOP-ITL5TP4M.localdomain>
2019-12-27 14:12:34 -0600

    ejemplo de routing en angularjs

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module('routingDemo', ['ngRoute']);
+
+        app.config(function ($routeProvider) {
+             
+            $routeProvider.when('/', {
+                templateUrl: '/login.html'
+                
+            }).when('/student/:username', {
+                templateUrl: '/student.html'
+                
+            }).otherwise({
+                redirectTo: "/"
+            });
+
+        app.controller("logincontroller", function ($scope, $location) {
+           
+            $scope.authenticate = function (username) {
+                // write authentication code here.. 
+
+                $location.path('/student/' + username)
+            };
+
+        });
+
+        app.controller("studentcontroller", function ($scope, $routeParams) {
+            $scope.username = $routeParams.username;
+        });
+
+    });
+ 
