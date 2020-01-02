6bac56a6099bad2d1ba2608bae3b528925e528dd
david llamas <david@stackcode.io>
2020-01-02 01:49:26 -0600

    manejo de routing en angular

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module('routingDemo', ['ngRoute']);
+
+        app.config(function ($routeProvider) {
+             
+            $routeProvider.when('/login', {
+                templateUrl: 'login.html',
+                controller:'logincontroller'
+            }).when('/student', {
+                templateUrl: 'student.html',
+                controller:'studentcontroller'
+            }).otherwise({
+                redirectTo: "/"
+            });
+
+       
+
+    });
+
+    app.controller("logincontroller", function ($scope, $location) {
+           
+        $scope.authenticate = function (username) {
+            // write authentication code here.. 
+
+            $location.path('/student/' + username)
+        };
+
+    }).controller("studentcontroller", function ($scope) {
+        $scope.nombre ="david llamas";
+    });
+ 
